7c9a555fe210401c4343504ecf204afe95256679
Nick Michael <nickmichael@live.co.uk>
2017-06-11 18:13:16 +0100

    Build script for windows app and installer

--- build/installer.js
+++
@@ -1,9 +0,0 @@
-const electronInstaller = require('electron-winstaller');
-const path = require('path');
-
-resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: '.\\dist\\app\\Razer-Paint-win32-ia32',
-    outputDirectory: '.\\dist\\app\\installer',
-    authors: 'Nick Michael',
-    exe: 'Razer Paint.exe'
-  });
